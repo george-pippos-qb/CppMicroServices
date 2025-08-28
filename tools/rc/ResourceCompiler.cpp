728b656db35c19f2366f8e7e7abded581bf3e068
George Pippos <george.pippos@quantum-brilliance.com>
2025-08-29 00:36:35 +1000

    Update boost/nowide submodule for clang19 build

--- tools/rc/ResourceCompiler.cpp
+++ tools/rc/ResourceCompiler.cpp
@@ -37,8 +37,8 @@
 #include <utility>
 #include <vector>
 
-#include <nowide/args.hpp>
-#include <nowide/fstream.hpp>
+#include <boost/nowide/args.hpp>
+#include <boost/nowide/fstream.hpp>
 
 #include "optionparser.h"
 #include "json/json.h"
@@ -170,7 +170,7 @@ namespace
     {
         try
         {
-            nowide::ifstream json(jsonFile);
+            boost::nowide::ifstream json(jsonFile);
             if (!json.is_open())
             {
                 throw std::runtime_error("Could not open file " + jsonFile);
@@ -391,7 +391,7 @@ ZipArchive::ZipArchive(std::string const& archiveFileName, int compressionLevel,
 {
     std::clog << "Initializing zip archive " << fileName << " ..." << std::endl;
     // clear the contents of a outFile if it exists
-    nowide::ofstream ofile(fileName, nowide::ofstream::trunc);
+    boost::nowide::ofstream ofile(fileName, boost::nowide::ofstream::trunc);
     ofile.close();
     if (!mz_zip_writer_init_file(writeArchive.get(), fileName.c_str(), 0))
     {
@@ -805,7 +805,7 @@ main(int argc, char** argv)
     setlocale(LC_ALL, "C.UTF-8")
 #endif
 
-        nowide::args _(argc, argv);
+        boost::nowide::args _(argc, argv);
 
     int const BUNDLE_MANIFEST_VALIDATION_ERROR_CODE(2);
 
@@ -931,8 +931,8 @@ main(int argc, char** argv)
         {
             validateManifestsInArchive(zipFile);
             std::string bundleBinaryFile(bundleFileOpt->arg);
-            nowide::ofstream outFileStream(bundleBinaryFile, std::ios::ate | std::ios::binary | std::ios::app);
-            nowide::ifstream zipFileStream(zipFile, std::ios::in | std::ios::binary);
+            boost::nowide::ofstream outFileStream(bundleBinaryFile, std::ios::ate | std::ios::binary | std::ios::app);
+            boost::nowide::ifstream zipFileStream(zipFile, std::ios::in | std::ios::binary);
             if (outFileStream.is_open() && zipFileStream.is_open())
             {
                 std::clog << "Appending file " << bundleBinaryFile << " with contents of resources zip file at "
@@ -943,7 +943,7 @@ main(int argc, char** argv)
                 // Depending on the ofstream destructor to close the file may result in a silent
                 // file write error. Hence the explicit call to close.
                 outFileStream.close();
-                if (outFileStream.rdstate() & nowide::ofstream::failbit)
+                if (outFileStream.rdstate() & boost::nowide::ofstream::failbit)
                 {
                     std::cerr << "Failed to write file : " << bundleBinaryFile << std::endl;
                     return_code = EXIT_FAILURE;
